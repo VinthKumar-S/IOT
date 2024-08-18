857662103bdb3056b9f060fbfd3d11fbbfb5a823
Vinithkumar-S <vk3228433@gmail.com>
2024-08-18 23:18:39 +0530

    add

--- Programs/flutter_esp32/lib/temperatureMeasurement.dart
+++ Programs/flutter_esp32/lib/temperatureMeasurement.dart
@@ -43,20 +43,125 @@ class temperatureMeasureState extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Weather Condition'),
+        backgroundColor: Colors.amber,
       ),
-      body: Center(
-        child: Obx((){
+      body:Obx((){
           double temperature=controller._temperature.value;
           double humidity=controller._humidity.value;
+          String weatherCondition ="No Rain";
+          if(humidity>85){
+            weatherCondition="Heavy rain will occure";
+          }
           return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Text('$temperature'),
-              Text('$humidity')
+              //Text('$temperature'),
+              //Text('$humidity')
+                Container(
+                  width: 500,
+                  height: 150,
+                  child: Center(
+                   /* child: Text(
+                      weatherCondition,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),*/
+                    child: Icon(
+                      Icons.wb_sunny,
+                      size: 130,
+                      color: Colors.white,
+                    ),
+                  ),
+                  decoration: BoxDecoration
+                  (
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(40),
+                      bottomRight: Radius.circular(40)
+                    ),
+                    boxShadow:[
+                      new BoxShadow(
+                        color: Colors.black,
+                        blurRadius: 15.0
+                      )
+                    ]
+                  ),
+                ),
+                SizedBox(height:150),
+                Table(
+                 // border: TableBorder.all(color: Colors.black),
+                 columnWidths: {
+                    0:FixedColumnWidth(200.0),
+                    1:FlexColumnWidth(),
+                    2:FixedColumnWidth(250.0)
+                 },
+                  children: [
+                    TableRow(
+                      decoration: BoxDecoration(
+                        //color: Colors.grey[300]
+                      ),
+                      children:[
+                          Column(
+                            children: [
+                              Text(
+                                "Temperature",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 23
+                                ),
+                              )
+                            ],
+                          ),
+                           Column(
+                            children: [
+                              Text(
+                                "Humidity",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 23
+                                ),
+                              )
+                            ],
+                          ),
+                      ] 
+                    ),
+                    TableRow(
+                      decoration: BoxDecoration(
+                        //color: Colors.grey[300]
+                      ),
+                      children:[
+                          Column(
+                            children: [
+                              SizedBox(height: 20,),
+                              Text(
+                                "$temperature",
+                                style: TextStyle(
+                                  fontSize: 22
+                                ),
+                              )
+                            ],
+                          ),
+                           Column(
+                            children: [
+                              SizedBox(height: 20,),
+                              Text(
+                                "$humidity",
+                                style: TextStyle(
+                                  fontSize: 22
+                                ),
+                              )
+                            ],
+                          ),
+                      ] 
+                    )
+                  ],
+                )
             ],
           );
         })
-      ),
     );
   }
 }
