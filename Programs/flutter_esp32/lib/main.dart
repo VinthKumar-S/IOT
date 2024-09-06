8b6339929b4807ffb2daf069d0e0a7b5099ca4d9
Vinithkumar-S <vk3228433@gmail.com>
2024-09-06 09:42:17 +0530

    add

--- Programs/flutter_esp32/lib/main.dart
+++ Programs/flutter_esp32/lib/main.dart
@@ -1,20 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_esp32/motorController.dart';
 import 'package:flutter_esp32/scheduleDates.dart';
 import 'package:flutter_esp32/temperatureMeasurement.dart';
+import 'package:workmanager/workmanager.dart';
+import 'package:flutter_esp32/notificationSerivce.dart';
 import 'firebase_options.dart';
 import 'package:get/get.dart';
 import 'bottomNavigator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Workmanager().initialize(callbackDispatcher);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(MyApp());
 }
 
+void callbackDispatcher(){
+  Workmanager().executeTask((taskName, inputData) async{
+    await checkForUpcomingSchedule();
+
+    return Future.value(true);
+  });
+}
+
+Future<void> checkForUpcomingSchedule() async{
+  var now = DateTime.now();
+  QuerySnapshot snapshot = await FirebaseFirestore.instance.collection("Schedule").get();
+
+  for(var doc in snapshot.docs){
+    DateTime eventDate = (doc['eventDate'] as Timestamp).toDate();
+
+    if(isSameDate(eventDate, now)){
+      NotificationService().showNotification(title: '$doc[eventName]',body: 'It Works');
+    }
+  }
+}
 
 class MyApp extends StatelessWidget {
   MyApp({super.key});
--- Programs/flutter_esp32/lib/scheduleDates.dart
+++ Programs/flutter_esp32/lib/scheduleDates.dart
@@ -2,7 +2,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_esp32/notificationSerivce.dart';
 import 'package:flutter_esp32/scheduleObj.dart';
 import 'package:get/get.dart';
 import 'scheduleOperation.dart';
